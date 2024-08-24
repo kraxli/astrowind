ebef7d32377d134b9c5be5be70a538d670293e92
kraxli <kraxli@pm.me>
2024-08-24 20:56:44 +0200

    adjust navigation bar

--- src/navigation.js
+++ src/navigation.js
@@ -85,35 +85,39 @@ export const headerData = {
     //     },
     //   ],
     // },
+    // {
+    //   text: 'Blog',
+    //   links: [
+    //     {
+    //       text: 'Blog List',
+    //       href: getBlogPermalink(),
+    //     },
+    //     {
+    //       text: 'Article',
+    //       href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+    //     },
+    //     {
+    //       text: 'Article (with MDX)',
+    //       href: getPermalink('markdown-elements-demo-post', 'post'),
+    //     },
+    //     {
+    //       text: 'Category Page',
+    //       href: getPermalink('tutorials', 'category'),
+    //     },
+    //     {
+    //       text: 'Tag Page',
+    //       href: getPermalink('astro', 'tag'),
+    //     },
+    //   ],
+    // },
     {
       text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
+      href: getPermalink(''),
     },
-    {
-      text: 'About us',
-      href: getPermalink('/about'),
-    },
+    // {
+    //   text: 'About us',
+    //   href: getPermalink('/about'),
+    // },
     {
       text: 'Contact',
       href: getPermalink('/contact'),
@@ -130,63 +134,83 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      // title: 'Contact',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        // { text: 'xxx@gmail.com', href: 'mailto:xxx@gmail.com' },
+        { text: '<b>Contact</b>', href: 'contact' },
       ],
-    },
+    },  
     {
-      title: 'Platform',
+      // title: 'Site',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        { text: '<b>Blog</b>', href: '' },
       ],
-    },
-    {
-      title: 'Support',
+    },  
+    { 
+      // title: 'Site',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
+        { text: '<b>About</b>', href: 'blog' },
       ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
+    }, 
+  //   {
+  //     title: 'Product',
+  //     links: [
+  //       { text: 'Features', href: '#' },
+  //       { text: 'Security', href: '#' },
+  //       { text: 'Team', href: '#' },
+  //       { text: 'Enterprise', href: '#' },
+  //       { text: 'Customer stories', href: '#' },
+  //       { text: 'Pricing', href: '#' },
+  //       { text: 'Resources', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Platform',
+  //     links: [
+  //       { text: 'Developer API', href: '#' },
+  //       { text: 'Partners', href: '#' },
+  //       { text: 'Atom', href: '#' },
+  //       { text: 'Electron', href: '#' },
+  //       { text: 'AstroWind Desktop', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Support',
+  //     links: [
+  //       { text: 'Docs', href: '#' },
+  //       { text: 'Community Forum', href: '#' },
+  //       { text: 'Professional Services', href: '#' },
+  //       { text: 'Skills', href: '#' },
+  //       { text: 'Status', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Company',
+  //     links: [
+  //       { text: 'About', href: '#' },
+  //       { text: 'Blog', href: '#' },
+  //       { text: 'Careers', href: '#' },
+  //       { text: 'Press', href: '#' },
+  //       { text: 'Inclusion', href: '#' },
+  //       { text: 'Social Impact', href: '#' },
+  //       { text: 'Shop', href: '#' },
+  //     ],
+  //   },
+  // ],
+  // secondaryLinks: [
+  //   { text: 'Terms', href: getPermalink('/terms') },
+  //   { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  // ],
+  // socialLinks: [
+  //   { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+  //   { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+  //   { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+  //   { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+  //   { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
-  socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
-  ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
+  footNote: `Copyright © 2024. Socionomics`
+  // footNote: `
+  //   <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
+  //   Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+  // `,
 };
